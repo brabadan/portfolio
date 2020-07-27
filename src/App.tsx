ca221fb39b91b1f9147cca971e92a3153e3da6f6
Rabadan Bakhmudov <brabadan@yandex.ru>
2020-07-27 23:34:08 +0300

    Portfolio: config sideMenu for Kubik, Templator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+import { SideMenu } from './side-menu/side-menu';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <SideMenu />
+        </Layout>
+    );
 }
 
 export default App;
---
+++ src/side-menu/side-menu.tsx
@@ -0,0 +1,42 @@
+﻿import React, { useState } from 'react';
+import { Menu } from 'antd';
+import { Layout } from 'antd';
+
+interface MenuItem {
+    name: string;
+    link: string;
+}
+
+export const SideMenu: React.FC = () => {
+    const menuItems: MenuItem[] = [
+        {
+            name: 'Kubik',
+            link: '../kubik/index.html'
+        },
+        {
+            name: 'templator',
+            link: '../templator/index.html'
+        }
+    ];
+
+    const [itemNo, setItemNo] = useState(0);
+
+    const handleClickMenu = (index: number) => () => {
+        setItemNo(index);
+    }
+
+    return (
+        <React.Fragment>
+            <Layout.Sider>
+                <Menu mode="inline">
+                    {menuItems.map((menuItem, index) => (
+                        <Menu.Item key={menuItem.name} onClick={handleClickMenu(index)}>{menuItem.name}</Menu.Item>
+                    ))}
+                </Menu>
+            </Layout.Sider>
+            <Layout.Content>
+                <iframe src={menuItems[itemNo].link} style={{ width: '100%', height: '99.5vh', border: 'none' }} />
+            </Layout.Content>
+        </React.Fragment>
+    )
+}
